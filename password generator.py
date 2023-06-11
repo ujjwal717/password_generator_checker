9c57dec722f0de87cb0308db03df34bb4c0d20bf
ujjwal717 <ujjwal717@gmail.com>
2023-06-11 17:57:14 +1300

    password generator intial py file

---
+++ password generator.py
@@ -0,0 +1,16 @@
+import re
+
+
+pattern = re.compile(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+string = input("Enter a valid password :- ")
+password = pattern.fullmatch(string)
+if password :
+
+ print("Password is valid :) ")
+else:
+    print("Sorry, password is not valid :( ")
+
+
+
+
+
